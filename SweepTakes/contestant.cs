6affe6f8d4551a7b393a7260a05a282e166ae919
rigo <rigobertoramirez606@gmail.com>
2018-06-25 07:41:46 -0500

    Dictionary

---
+++ SweepTakes/contestant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepTakes
+{
+    class contestant
+    {
+
+        public class Contestant
+        {
+            private string firstName;
+            private string lastName;
+            private string email;
+            private string adress;
+            private string registrationNumber;
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Adress { get; set; }
+            public string RegistrationNumber { get; set; }
+
+            public void DisplayFirstLastName()
+            {
+                Dictionary<string, string> FullName = new Dictionary<string, string>();
+                
+            }
+
+
+        }
+    }
+}
